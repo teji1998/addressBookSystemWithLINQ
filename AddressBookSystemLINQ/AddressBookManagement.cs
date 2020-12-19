61ed832190024f05ae6d1a4ee1f4509ac336c55c
“teji1998” <tejasvinirpk@gmail.com>
2020-12-19 16:53:31 +0530

    Added:Code to create a new data table

---
+++ AddressBookSystemLINQ/AddressBookManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookSystemLINQ
+{
+    public class AddressBookManagement
+    {
+        //creating a datatable
+        DataTable dataTable = new DataTable();
+    }
+}
