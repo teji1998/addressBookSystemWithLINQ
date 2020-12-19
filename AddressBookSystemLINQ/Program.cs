57753d3d93c272f4ad989afded8a93cb4289fcc3
“teji1998” <tejasvinirpk@gmail.com>
2020-12-19 15:15:56 +0530

    Added: welcome message

--- AddressBookSystemLINQ/Program.cs
+++ AddressBookSystemLINQ/Program.cs
@@ -6,7 +6,7 @@ namespace AddressBookSystemLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to address book problem using LINQ !!!!");
         }
     }
 }
