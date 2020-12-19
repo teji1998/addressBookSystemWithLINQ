1ef8be818997f04968abcb322af2ba93d03446ca
“teji1998” <tejasvinirpk@gmail.com>
2020-12-19 19:03:36 +0530

    Added:Code to insert values into the data table

--- AddressBookSystemLINQ/AddressBookManagement.cs
+++ AddressBookSystemLINQ/AddressBookManagement.cs
@@ -22,6 +22,13 @@ namespace AddressBookSystemLINQ
             addressBookTable.Columns.Add("MobileNumber", typeof(string));
             addressBookTable.Columns.Add("EmailId", typeof(string));
 
+            addressBookTable.Rows.Add("Tejaswini", "Kulkarni", "Chembur", "Mumbai", "Maharashtra", 400071, "9920275347", "teju@gmail.com");
+            addressBookTable.Rows.Add("Sravani", "Sabbisetti", "Gandhinagar", "Hyderabad", "Telangana", 458000, "9927893400", "sravani_123@abc.com");
+            addressBookTable.Rows.Add("Bhagyalakshmi", "Reddy", "JPnagar", "Hyderabad", "Telangana", 580000, "8097456100", "bhagya123@yahoo.com");
+            addressBookTable.Rows.Add("Lee", "Minho", "Gangwon", "Seoul", "South Korea", 689768, "9765123000", "leemin123@kbs.com");
+            addressBookTable.Rows.Add("Abhimanyu", "Jagirdar", "Juhu", "Pune", "Maharashtra", 400098, "8904543432", "abhiJagi987@cms.com");
+            addressBookTable.Rows.Add("Jaydeep", "Patil", "Malleshwaram", "Pune", "Karnataka", 560789, "8097395111", "jay_patil@sm.com");
+
         }
     }
 }
