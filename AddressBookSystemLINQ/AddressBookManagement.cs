ab04c97cf9d6678cde1d95bda7900c69116bea59
“teji1998” <tejasvinirpk@gmail.com>
2020-12-19 17:14:44 +0530

    Added:Columns into data table

--- AddressBookSystemLINQ/AddressBookManagement.cs
+++ AddressBookSystemLINQ/AddressBookManagement.cs
@@ -8,6 +8,20 @@ namespace AddressBookSystemLINQ
     public class AddressBookManagement
     {
         //creating a datatable
-        DataTable dataTable = new DataTable();
+        DataTable addressBookTable = new DataTable();
+        
+        public AddressBookManagement()
+        {
+            //Adding columns into data table
+            addressBookTable.Columns.Add("FirstName", typeof(string));
+            addressBookTable.Columns.Add("LastName", typeof(string));
+            addressBookTable.Columns.Add("Address", typeof(string));
+            addressBookTable.Columns.Add("City", typeof(string));
+            addressBookTable.Columns.Add("State", typeof(string));
+            addressBookTable.Columns.Add("Zip", typeof(int));
+            addressBookTable.Columns.Add("MobileNumber", typeof(string));
+            addressBookTable.Columns.Add("EmailId", typeof(string));
+
+        }
     }
 }
